1d8b8fa9a6bcf5bb44c44cc636b4661c799eca76
milestone-datascience <datascience.milestone@gmail.com>
2026-09-01 19:35:17 +0530

    Added new Files

---
+++ MyDjangoProject/views.py
@@ -0,0 +1,11 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+  return render(request,template_name="home.html")
+
+def about(request):
+  return render(request,template_name="about.html")
+
+def post(request):
+  return render(request,template_name="post.html")
