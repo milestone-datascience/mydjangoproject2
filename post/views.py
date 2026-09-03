712e5f1347b5959c6643c2d4637b78df3ae286f7
milestone-datascience <datascience.milestone@gmail.com>
2026-09-03 18:51:07 +0530

    Updated 3

--- post/views.py
+++ post/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def post(request):
-  return render(request,template_name="post.html")
+  data = [
+    "This is the post about Java",
+    "This is the post about Javascript",
+    "This is the post about HTML",
+    "This is the post about CSS",
+    "This is the post about Python",
+    "This is the post about Django"
+    ]
+  
+  return render(request,template_name="post.html",context={"data":data})
