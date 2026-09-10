d974f7a4cb5e82b90d8589ce1b47047690caa2b5
milestone-datascience <datascience.milestone@gmail.com>
2026-09-10 18:48:44 +0530

    10-09-2026

---
+++ 10-09-2026/MyDjangoProject/views.py
@@ -0,0 +1,9 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+  return render(request,template_name="home.html")
+
+def about(request):
+  return render(request,template_name="about.html")
+
