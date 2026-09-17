3f9eb8a1e56a7e7cab0c7746b6e04fbb166d3ab3
milestone-datascience <datascience.milestone@gmail.com>
2026-09-17 19:28:00 +0530

    updated 17

--- 17-09-2026/post/views.py
+++ 17-09-2026/post/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post(request):
@@ -12,7 +13,12 @@ def post(request):
   #   "This is the post about Django"
   #   ]
 
-  data = Post.objects.all()
-  
-  
-  return render(request,template_name="post.html",context={"data":data})
+  if request.method == "GET":
+    form = PostForm()
+    data = Post.objects.all()
+    return render(request,template_name="post.html",context={"data":data,"form":form})
+  elif request.method == 'POST':
+    form = PostForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect("/posts/")
